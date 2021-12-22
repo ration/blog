d19d295dc3d947147b26f0a31f12e2a38dee26f5
Tatu Lahtela <lahtela@iki.fi>
2021-12-22 13:10:24 +0200

    Exclude patterns, url

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export JEKYLL_ENV=production bundle exec jekyll build
-rsync -crvz --rsh='ssh -p22' --delete-after --delete-excluded  _site/ blog_host:~/www/
+rsync -crvz --rsh='ssh -p22' --delete-after --delete --exclude "tt" --exclude "other" _site/ blog_host1:~/www/
